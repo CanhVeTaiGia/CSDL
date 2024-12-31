3f3b7f4ee3ab8f18f11d319cc90adbd2d0f842f3
CanhVeTaiGia <lvy16012005@gmail.com>
2024-12-31 14:49:25 +0700

    add

--- BTVN9/bai2.c
+++ BTVN9/bai2.c
@@ -3,41 +3,53 @@
 
 #define MAX 100
 
-void initMatrix(int matrix[MAX][MAX], int numVertices) {
-    for (int i = 0; i < numVertices; i++) {
-        for (int j = 0; j < numVertices; j++) {
+void initMatrix(int matrix[MAX][MAX], int numVertices)
+{
+    for (int i = 0; i < numVertices; i++)
+    {
+        for (int j = 0; j < numVertices; j++)
+        {
             matrix[i][j] = 0;
         }
     }
 }
 
-void addEdge(int matrix[MAX][MAX], int start, int end) {
+void addEdge(int matrix[MAX][MAX], int start, int end)
+{
     matrix[start][end] = 1;
     matrix[end][start] = 1;
 }
 
-void printMatrix(int matrix[MAX][MAX], int numVertices) {
+void printMatrix(int matrix[MAX][MAX], int numVertices)
+{
     printf("Ma tran ke cua do thi:\n");
-    for (int i = 0; i < numVertices; i++) {
-        for (int j = 0; j < numVertices; j++) {
+    for (int i = 0; i < numVertices; i++)
+    {
+        for (int j = 0; j < numVertices; j++)
+        {
             printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
 }
 
-int checkSymmetry(int matrix[MAX][MAX], int numVertices) {
-    for (int i = 0; i < numVertices; i++) {
-        for (int j = 0; j < numVertices; j++) {
-            if (matrix[i][j] != matrix[j][i]) {
-                return 0; 
+int checkSymmetry(int matrix[MAX][MAX], int numVertices)
+{
+    for (int i = 0; i < numVertices; i++)
+    {
+        for (int j = 0; j < numVertices; j++)
+        {
+            if (matrix[i][j] != matrix[j][i])
+            {
+                return 0;
             }
         }
     }
-    return 1;  
+    return 1;
 }
 
-int main() {
+int main()
+{
     int numVertices, numEdges, start, end;
     int matrix[MAX][MAX];
     printf("Nhap so luong dinh cua do thi: ");
@@ -46,19 +58,22 @@ int main() {
     scanf("%d", &numEdges);
     initMatrix(matrix, numVertices);
     printf("Nhap danh sach cac canh cua do thi:\n");
-    for (int i = 0; i < numEdges; i++) {
+    for (int i = 0; i < numEdges; i++)
+    {
         printf("Nhap canh %d (dinh dau, dinh cuoi): ", i + 1);
         scanf("%d %d", &start, &end);
         addEdge(matrix, start, end);
     }
     printMatrix(matrix, numVertices);
 
-    if (checkSymmetry(matrix, numVertices)) {
+    if (checkSymmetry(matrix, numVertices))
+    {
         printf("Ma tran ke la doi xung.\n");
-    } else {
+    }
+    else
+    {
         printf("Ma tran ke khong doi xung.\n");
     }
 
     return 0;
 }
-
--- BTVN9/bai3.c
+++ BTVN9/bai3.c
@@ -3,67 +3,85 @@
 
 #define MAX 100
 
-typedef struct {
+typedef struct
+{
     int adjMatrix[MAX][MAX];
     int numVertices;
 } GraphMatrix;
 
-typedef struct Node {
+typedef struct Node
+{
     int vertex;
-    struct Node* next;
+    struct Node *next;
 } Node;
 
-typedef struct {
-    Node* adj[MAX];
+typedef struct
+{
+    Node *adj[MAX];
     int numVertices;
 } GraphList;
 
-void initGraphMatrix(GraphMatrix* graph, int numVertices) {
+void initGraphMatrix(GraphMatrix *graph, int numVertices)
+{
     graph->numVertices = numVertices;
-    for (int i = 0; i < numVertices; i++) {
-        for (int j = 0; j < numVertices; j++) {
+    for (int i = 0; i < numVertices; i++)
+    {
+        for (int j = 0; j < numVertices; j++)
+        {
             graph->adjMatrix[i][j] = 0;
         }
     }
 }
 
-void initGraphList(GraphList* graph, int numVertices) {
+void initGraphList(GraphList *graph, int numVertices)
+{
     graph->numVertices = numVertices;
-    for (int i = 0; i < numVertices; i++) {
+    for (int i = 0; i < numVertices; i++)
+    {
         graph->adj[i] = NULL;
     }
 }
 
-void addEdgeMatrix(GraphMatrix* graph, int start, int end, int isDirected) {
+void addEdgeMatrix(GraphMatrix *graph, int start, int end, int isDirected)
+{
     graph->adjMatrix[start][end] = 1;
-    if (!isDirected) {
+    if (!isDirected)
+    {
         graph->adjMatrix[end][start] = 1;
     }
 }
 
-void addEdgeList(GraphList* graph, int start, int end, int isDirected) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
+void addEdgeList(GraphList *graph, int start, int end, int isDirected)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
     newNode->vertex = end;
     newNode->next = graph->adj[start];
     graph->adj[start] = newNode;
 
-    if (!isDirected) {
-        newNode = (Node*)malloc(sizeof(Node));
+    if (!isDirected)
+    {
+        newNode = (Node *)malloc(sizeof(Node));
         newNode->vertex = start;
         newNode->next = graph->adj[end];
         graph->adj[end] = newNode;
     }
 }
 
-int DFS_Matrix(int vertex, int visited[], int recursionStack[], GraphMatrix* graph) {
+int DFS_Matrix(int vertex, int visited[], int recursionStack[], GraphMatrix *graph)
+{
     visited[vertex] = 1;
     recursionStack[vertex] = 1;
 
-    for (int i = 0; i < graph->numVertices; i++) {
-        if (graph->adjMatrix[vertex][i] == 1) {
-            if (!visited[i] && DFS_Matrix(i, visited, recursionStack, graph)) {
+    for (int i = 0; i < graph->numVertices; i++)
+    {
+        if (graph->adjMatrix[vertex][i] == 1)
+        {
+            if (!visited[i] && DFS_Matrix(i, visited, recursionStack, graph))
+            {
                 return 1;
-            } else if (recursionStack[i]) {
+            }
+            else if (recursionStack[i])
+            {
                 return 1;
             }
         }
@@ -73,16 +91,21 @@ int DFS_Matrix(int vertex, int visited[], int recursionStack[], GraphMatrix* gra
     return 0;
 }
 
-int DFS_List(int vertex, int visited[], int recursionStack[], GraphList* graph) {
+int DFS_List(int vertex, int visited[], int recursionStack[], GraphList *graph)
+{
     visited[vertex] = 1;
     recursionStack[vertex] = 1;
 
-    Node* temp = graph->adj[vertex];
-    while (temp != NULL) {
+    Node *temp = graph->adj[vertex];
+    while (temp != NULL)
+    {
         int neighbor = temp->vertex;
-        if (!visited[neighbor] && DFS_List(neighbor, visited, recursionStack, graph)) {
+        if (!visited[neighbor] && DFS_List(neighbor, visited, recursionStack, graph))
+        {
             return 1;
-        } else if (recursionStack[neighbor]) {
+        }
+        else if (recursionStack[neighbor])
+        {
             return 1;
         }
         temp = temp->next;
@@ -91,30 +114,37 @@ int DFS_List(int vertex, int visited[], int recursionStack[], GraphList* graph) 
     recursionStack[vertex] = 0;
     return 0;
 }
-int detectCycleMatrix(GraphMatrix* graph) {
+int detectCycleMatrix(GraphMatrix *graph)
+{
     int visited[MAX] = {0};
     int recursionStack[MAX] = {0};
 
-    for (int i = 0; i < graph->numVertices; i++) {
-        if (!visited[i] && DFS_Matrix(i, visited, recursionStack, graph)) {
+    for (int i = 0; i < graph->numVertices; i++)
+    {
+        if (!visited[i] && DFS_Matrix(i, visited, recursionStack, graph))
+        {
             return 1;
         }
     }
     return 0;
 }
-int detectCycleList(GraphList* graph) {
+int detectCycleList(GraphList *graph)
+{
     int visited[MAX] = {0};
     int recursionStack[MAX] = {0};
 
-    for (int i = 0; i < graph->numVertices; i++) {
-        if (!visited[i] && DFS_List(i, visited, recursionStack, graph)) {
+    for (int i = 0; i < graph->numVertices; i++)
+    {
+        if (!visited[i] && DFS_List(i, visited, recursionStack, graph))
+        {
             return 1;
         }
     }
     return 0;
 }
 
-int main() {
+int main()
+{
     int numVertices, numEdges, start, end, isDirected, choice;
     GraphMatrix graphMatrix;
     GraphList graphList;
@@ -126,35 +156,47 @@ int main() {
 
     printf("Chon loai do thi (1: Ma tran ke, 2: Danh sach ke): ");
     scanf("%d", &choice);
-    if (choice == 1) {
+    if (choice == 1)
+    {
         initGraphMatrix(&graphMatrix, numVertices);
-    } else if (choice == 2) {
+    }
+    else if (choice == 2)
+    {
         initGraphList(&graphList, numVertices);
     }
     printf("Nhap danh sach cac canh:\n");
-    for (int i = 0; i < numEdges; i++) {
+    for (int i = 0; i < numEdges; i++)
+    {
         scanf("%d %d", &start, &end);
         printf("La do thi co huong? (1: Co huong, 0: Vo huong): ");
         scanf("%d", &isDirected);
-        if (choice == 1) {
+        if (choice == 1)
+        {
             addEdgeMatrix(&graphMatrix, start, end, isDirected);
-        } else {
+        }
+        else
+        {
             addEdgeList(&graphList, start, end, isDirected);
         }
     }
     int hasCycle = 0;
-    if (choice == 1) {
+    if (choice == 1)
+    {
         hasCycle = detectCycleMatrix(&graphMatrix);
-    } else {
+    }
+    else
+    {
         hasCycle = detectCycleList(&graphList);
     }
 
-    if (hasCycle) {
+    if (hasCycle)
+    {
         printf("Do thi co chu ky.\n");
-    } else {
+    }
+    else
+    {
         printf("Do thi khong co chu ky.\n");
     }
 
     return 0;
 }
-
